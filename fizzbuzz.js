33c0bd2a71f944195dce0fa53a2f6a71b0265fd3
mike <mdaniel0119@skilledkc.org>
2020-11-03 07:38:55 -0600

    Finished

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+function fizzbuzz 
+
+for(i = 0; i < 100; i++){
+    if( i === 0 )
+       console.log(0);
+else if( i % 3 === 0 && i % 5 === 0)
+    //print fizzbuzz
+    console.log("fizzbuzz").innerhtml;
+else if( i % 3 === 0 )
+    //print fizz
+    console.log("fizz").innerhtml;
+
+else if(i % 5 === 0)
+    //print buzz
+    console.log("buzz").innerhtml;
+
+else
+    //print i 
+    console.log(i);
+}
